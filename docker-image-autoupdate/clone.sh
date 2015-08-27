578e0a3dcfa91186b961b1e9e2ff55845659d5d2
Santiago Saavedra <santiagosaavedra@gmail.com>
2015-08-27 17:31:44 +0200

    Add host key to known hosts (should this be in git?)

--- docker-image-autoupdate/clone.sh
+++ docker-image-autoupdate/clone.sh
@@ -22,6 +22,13 @@ if [ ! -f ~/.ssh/id_rsa.pub ]; then
 	cat ~/.ssh/id_rsa.pub
 fi
 
+if [ ! -f ~/.ssh/known_hosts ]; then
+	cat >~/.ssh/known_hosts <<EOF
+|1|rfC6u5XrgzfGX7XO5akLVmjkKBQ=|jg24HNIhC/QXY0MqOkekSNvL4zk= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCJy+03h9EVtECremT4ZWJ68TcELJdhY7PW0iP6euHzF5vVscGhpzLg97vGTHH1c95PC1zQnjv+wzohH/rtFecNMCaZ11Q4kNAm96VJEgux7x+guaSKazRlH7x/qf9DPPvh+Wd3KPjI0/FtYN9spMq76SWD6dRJgm/g4v9pSFLtXQ==
+|1|y4iHteiZxg6sfLHVl/E/ZT8UIYk=|+zTePWmboDmV6GNhCtAwQ3ktFXQ= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCJy+03h9EVtECremT4ZWJ68TcELJdhY7PW0iP6euHzF5vVscGhpzLg97vGTHH1c95PC1zQnjv+wzohH/rtFecNMCaZ11Q4kNAm96VJEgux7x+guaSKazRlH7x/qf9DPPvh+Wd3KPjI0/FtYN9spMq76SWD6dRJgm/g4v9pSFLtXQ==
+EOF
+fi
+
 if [ "x${REDIS_PORT}" != "x" ]; then
 	echo "Using Redis brain at ${REDIS_PORT}"
 	export REDIS_URL="${REDIS_PORT}"
